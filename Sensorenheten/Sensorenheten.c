82973c54f9ba2669064543de915dbca36b4a8f5e
Tobias Norlund <tobno568@student.liu.se>
2013-04-10 13:49:32 +0200

    First commit

---
+++ Sensorenheten/Sensorenheten.c
@@ -0,0 +1,17 @@
+﻿/*
+ * Sensorenheten.c
+ *
+ * Created: 4/10/2013 1:47:53 PM
+ *  Author: tobno568
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
