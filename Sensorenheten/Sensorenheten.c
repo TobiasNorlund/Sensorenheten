c049341a12749e7a317d006f253e32516967fbae
Tobias Norlund <tobno568@student.liu.se>
2013-04-10 17:28:28 +0200

    Kan avkoda en sensor

--- Sensorenheten/Sensorenheten.c
+++ Sensorenheten/Sensorenheten.c
@@ -7,11 +7,38 @@
 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include "Sensorenheten.h"
+
 
 int main(void)
 {
+	DDRA = 0b11101111;
+	DDRB = 0b10000000;
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); 
+	ADMUX |= (1 << REFS0); 
+	ADMUX |= (1 << MUX2) | (0 << MUX1) | (0 << MUX0); //ADC på PA4 för test 	
+	ADMUX |= (1 << ADLAR); 
+	ADCSRA |= (1 << ADEN); 
+	ADCSRA |= (1 << ADIE);
+	sei();
+	ADCSRA |= (1 << ADSC); 
     while(1)
     {
-        //TODO:: Please write your application code 
+        //TODO för MSPI
     }
+}
+
+ISR(ADC_vect)
+{
+	//case multplexer = ??
+	PORTA = ADCH & 0b11101111;
+	//PORTB = ADCH & 0b00010000;
+
+	Vout = 20.99*ADCH + 0.19; //Tillfällig linjär ekv.
+	//ADEN=0
+	//if(ADMUX=7)
+	//ADMUX=0
+	//else ADMUX=ADMUX+1
+	ADCSRA |= (1 << ADSC); 
 }
---
+++ Sensorenheten/Sensorenheten.h
@@ -0,0 +1,20 @@
+﻿/*
+ * Sensorenheten.h
+ *
+ * Created: 4/10/2013 2:39:25 PM
+ *  Author: tobno568
+ */ 
+
+int sensor1 [10];
+int sensor2 [10];
+int sensor3 [10];
+int sensor4 [10];
+int sensor5 [10];
+int sensor6 [10];
+int sensor7 [10];
+int sensor8 [10];
+int gyro [10];
+
+int debugBuffer [];
+
+int Vout;
